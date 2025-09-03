c9822f272283e0ae34adffcb7f00359b84a92bb2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-02 23:42:57 -0400

    install deps in dedicated script

---
+++ get-dependencies.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt6ct            \
+	qt6-wayland      \
+	strawberry       \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-common --prefer-nano
