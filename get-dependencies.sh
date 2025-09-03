06de6b74b3392e6a7daf9329e14e3800af674b23
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-03 04:52:09 +0000

    refactor, use wrapper scripts, add `aarch64` releases (#15)

---
+++ get-dependencies.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt6ct            \
+	qt6-wayland      \
+	strawberry       \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-mesa --prefer-nano qt6-base-mini libxml2-mini opus-mini
+
+pacman -Q strawberry | awk '{print $2; exit}' > ~/version
--- strawberry-appimage.sh
+++ strawberry-appimage.sh
@@ -1,92 +1,31 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
-PACKAGE=strawberry
-DESKTOP=org.strawberrymusicplayer.strawberry.desktop
-ICON=strawberry.png
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+VERSION="$(cat ~/version)"
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
-echo "$VERSION" > ~/version
-
-UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
-URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
-
-# Prepare AppDir
-mkdir -p ./AppDir/shared/lib
-cd ./AppDir
-
-cp -v /usr/share/applications/"$DESKTOP"            ./
-cp -v /usr/share/icons/hicolor/128x128/apps/"$ICON" ./
-cp -v /usr/share/icons/hicolor/128x128/apps/"$ICON" ./.DirIcon
+export ADD_HOOKS="self-updater.bg.hook"
+export DESKTOP=/usr/share/applications/org.strawberrymusicplayer.strawberry.desktop
+export ICON=/usr/share/icons/hicolor/128x128/apps/strawberry.png
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=strawberry-"$VERSION"-anylinux-"$ARCH".AppImage
+export DEPLOY_OPENGL=1
+export DEPLOY_PIPEWIRE=1
+export DEPLOY_GSTREAMER=1
 
 # ADD LIBRARIES
-wget "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -a -- ./lib4bin -p -v -s -k -e \
-	/usr/bin/strawberry* \
-	/usr/lib/libgst* \
-	/usr/lib/gstreamer-*/*.so \
-	/usr/lib/qt6/plugins/iconengines/* \
-	/usr/lib/qt6/plugins/imageformats/* \
-	/usr/lib/qt6/plugins/platforms/* \
-	/usr/lib/qt6/plugins/platformthemes/* \
-	/usr/lib/qt6/plugins/styles/* \
-	/usr/lib/qt6/plugins/sqldrivers/* \
-	/usr/lib/qt6/plugins/tls/* \
-	/usr/lib/qt6/plugins/xcbglintegrations/* \
-	/usr/lib/qt6/plugins/wayland-*/* \
-	/usr/lib/pulseaudio/* \
-	/usr/lib/pipewire-*/* \
-	/usr/lib/spa-*/*/*
-
-# DEPLOY GSTREAMER
-echo "Deploying Gstreamer binaries..."
-cp -vn /usr/lib/gstreamer-*/*  ./shared/lib/gstreamer-* || true
-
-# Patch a relative interpreter for the gstreamer plugins
-echo "Sharunning Gstreamer bins..."
-rm -f ./shared/lib/gstreamer-1.0/libgstopengl* || true
-for plugin in ./shared/lib/gstreamer-1.0/gst-*; do
-	if file "$plugin" | grep -i 'elf.*executable'; then
-		mv "$plugin" ./shared/bin && ln -s ../../../sharun "$plugin"
-		echo "Sharan $plugin"
-	else
-		echo "$plugin is not a binary, skipping..."
-	fi
-done
-
-# Prepare sharun
-ln ./sharun ./AppRun
-./sharun -g
-
-# Remove bloats
-echo "Removing bloats..."
-rm -f ./shared/lib/libLLVM* ./shared/lib/libgallium* || true
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/strawberry*
 
 # MAKE APPIMAGE WITH URUNTIME
-cd ..
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
 
-#Add udpate info to runtime
-echo "Adding update information \"$UPINFO\" to runtime..."
-printf "$UPINFO" > data.upd_info
-llvm-objcopy --update-section=.upd_info=data.upd_info \
-	--set-section-flags=.upd_info=noload,readonly ./uruntime
-printf 'AI\x02' | dd of=./uruntime bs=1 count=3 seek=8 conv=notrunc
-
-echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime \
-	-i ./AppDir -o "$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
-
-echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
-echo "All Done!"
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
+mv -v ~/version     ./dist
