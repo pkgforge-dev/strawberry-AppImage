2b6f56d9d7433772e646d4bfdb3851119de72b3c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-03 00:02:15 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -30,6 +30,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+./get-debloated-pkgs.sh --add-mesa --prefer-nano qt6-base-mini libxml2-mini opus-mini
 
 pacman -Q strawberry | awk '{print $2; exit}' > ~/version
