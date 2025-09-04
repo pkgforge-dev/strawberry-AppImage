de3049c5f6aa94f4c7c30970ca132093b9945f73
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-03 22:21:42 -0400

    no need to add vulkan

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,6 +31,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa --prefer-nano qt6-base-mini libxml2-mini opus-mini
+./get-debloated-pkgs.sh --add-opengl --prefer-nano qt6-base-mini libxml2-mini opus-mini
 
 pacman -Q strawberry | awk '{print $2; exit}' > ~/version
