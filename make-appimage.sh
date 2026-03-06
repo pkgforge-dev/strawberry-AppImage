a3da98ad187fa4048d90923cb507841c393fd64a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-06 05:57:58 -0400

    add missing step

--- make-appimage.sh
+++ make-appimage.sh
@@ -19,6 +19,9 @@ quick-sharun /usr/bin/strawberry*
 
 # Additional changes can be done in between here
 
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
 quick-sharun --test ./dist/*.AppImage
